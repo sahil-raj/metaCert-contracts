26bc4189367a86e4ffddcd980a539123b20a605b
sahil-raj <sahilrajaaru@gmail.com>
2024-05-24 23:00:12 +0530

    done/2

--- contracts/MetaCert.sol
+++ contracts/MetaCert.sol
@@ -11,10 +11,15 @@ contract MetaCert is Ownable, ERC721URIStorage {
 
     constructor() Ownable(msg.sender) ERC721("MetaCert certs", "MCC") {}
 
+    //events
+    event certMinted(uint256 _tokenUid);
+
     //state vars
     struct Issuer {
         address account_address;
         string name;
+        string issuer_address;
+        uint256 issuer_uid_govt;
     }
 
     uint256 private _nextTokenId = 0;
@@ -23,9 +28,10 @@ contract MetaCert is Ownable, ERC721URIStorage {
     mapping(uint256 => Issuer) public IssuerMapping;
 
     //modifiers
-    modifier registerIssuerMod(string memory _name, uint256 _isVerified) {
+    modifier registerIssuerMod(string memory _name, string memory _issuerAddress, uint256 _issuerUidGovt, uint256 _isVerified) {
         require(_isVerified == 1, "Issuer must be verified");
         require(bytes(_name).length > 0, "Issuer must have a name");
+        require(bytes(_issuerAddress).length>0, "issuer physical address is required");
         _;
     }
 
@@ -36,9 +42,9 @@ contract MetaCert is Ownable, ERC721URIStorage {
     }
 
     //functions
-    function registerIssuer(string memory _name, uint256 _isVerified) registerIssuerMod(_name, _isVerified) public returns(uint256) {
+    function registerIssuer(string memory _name, string memory _issuerAddress, uint256 _issuerUidGovt, uint256 _isVerified) registerIssuerMod(_name, _issuerAddress, _issuerUidGovt, _isVerified) public returns(uint256) {
 
-        Issuer memory i = Issuer(msg.sender, _name);
+        Issuer memory i = Issuer(msg.sender, _name, _issuerAddress, _issuerUidGovt);
         IssuerMapping[_nextIssuerUid] = i;
         _nextIssuerUid = _nextIssuerUid+1;
 
@@ -48,11 +54,12 @@ contract MetaCert is Ownable, ERC721URIStorage {
     function mintCert(uint256 _issuerUid, address _to, string calldata _uri) mintCertMod(_issuerUid, _to, _uri) public returns(bool) {
         _mint(_to, _nextTokenId);
         _setTokenURI(_nextTokenId, _uri);
+        emit certMinted(_nextTokenId);
         _nextTokenId = _nextTokenId+1;
         return true;
     }
 
-    function verifyCert(address _addHolder, uint256 _tokenUid) public returns(bool) {
+    function verifyCert(address _addHolder, uint256 _tokenUid) public view returns(bool) {
         if (ownerOf(_tokenUid) == _addHolder) {
             return true;
         } else {
